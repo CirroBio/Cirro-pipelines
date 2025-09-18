02da50112530469f479581e6664cf38232ddf80e
Nathan Thorpe <nathan@techstormpc.com>
2025-09-18 14:47:55 -0700

    support multiple lanes

--- nf-core/demux/1.0/preprocess.py
+++ nf-core/demux/1.0/preprocess.py
@@ -19,7 +19,7 @@ def make_manifest(ds: PreprocessDataset) -> pd.DataFrame:
     """
 
     flowcell_id = ds.params.get('flowcell_id') or ds.metadata['dataset']['name']
-    lane = ds.params.get('lane')
+    lanes = ds.params.get('lane').split(',')
     samplesheet_path = ds.params.get('samplesheet')
 
     # Try to find the correct run directory dataset
@@ -41,6 +41,7 @@ def make_manifest(ds: PreprocessDataset) -> pd.DataFrame:
             'lane': lane,
             'flowcell': run_dir
         }
+        for lane in lanes
     ])
     return manifest
 
